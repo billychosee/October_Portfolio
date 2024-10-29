4d0ee6482d83dd3a94d57a9a406e8474fb65b6f6
Billy <billychosee@gmail.com>
2024-10-29 22:05:03 +0200

    first commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building Austech Insure Website",
+    description: "Insurance",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Team Van Life",
+    des: "Experience the thrill of adventure with Van Life! Our cozy and reliable vans are perfect for your next road trip, providing the freedom of the open road. Create lasting memories and explore new destinations with Van Life today!",
+    img: "/Team_Van_Life.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://van-life-complete.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Tailwind Landing Page",
+    des: "Stay connected with Leicester City Football Club. Get updates and engage with fellow fans effortlessly",
+    img: "/Taiwind_project.svg",
+    iconLists: ["/tail.svg", "/ts.svg"],
+    link: "https://billychosee.github.io/1st_Tailwind_Project/",
+  },
+  {
+    id: 3,
+    title: "Air BnB",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/Travel_Journal.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://react-project-1-gamma.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Austech Insure",
+    des: "Revamped the Austech Insure website by integrating dynamic GSAP animations and immersive Three.js 3D effects, enhancing the user experience and showcasing our insurance solutions in an engaging way.",
+    img: "/Austech_Insure.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg",],
+    link: "https://github.com/billychosee",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Billy was a great experience. His professionalism, quick responses, and dedication to high-quality work shone through at every stage of the project. Billy’s enthusiasm for development is genuinely impressive. If you’re aiming to elevate your website and brand, Billy is an outstanding choice.",
+    name: "Derrick Deshu",
+    title: "Deshoe.inc",
+  },
+  {
+    quote:
+      "Collaborating with Billy was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Billy's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Billy is the ideal partner.",
+    name: "Nick Mutemasango",
+    title: "Stipilipi tech",
+  },
+  {
+    quote:
+      "Working with Billy was a fantastic experience. His professionalism, prompt communication, and commitment to high-quality results were evident throughout the entire project. Billy’s passion for development is truly remarkable. If you're looking to enhance your website and brand, Billy is an excellent choice.",
+    name: "Meekness Kaboti",
+    title: "Director of Kabo Billiards",
+  },
+  {
+    quote:
+      "Working with Billy was an exceptional experience. His professionalism, prompt replies, and commitment to delivering high-quality work were evident throughout the entire project. Billy’s passion for development is truly remarkable. If you want to enhance your website and brand, Billy is definitely a top-notch choice",
+    name: "Caleb Gondo",
+    title: "Gondo designs",
+  },
+  {
+    quote:
+      "Collaborating with Billy was a pleasure. His professionalism, responsiveness, and dedication to delivering high-quality work stood out at every step. Billy’s enthusiasm for development is impressive, making him an exceptional choice for anyone looking to elevate their website and brand.",
+    name: "Tapiwa Muchena",
+    title: "Head Technologies",
+  },
+];
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "cloudinary",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "appwrite",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Austech Dynamite Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - Uncommon.org",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/billychosee",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "#",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/billy-kadyamatimba-a53a0a2b7",
+  },
+];
