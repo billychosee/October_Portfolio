b76cc2c1e23a871e72bdbe19780a2e9638bd0606
Billy <billychosee@gmail.com>
2024-10-30 08:50:51 +0200

    MetaData link

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Billy'S Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  description: "Modern Portfolio",
 };
 
 export default function RootLayout({
